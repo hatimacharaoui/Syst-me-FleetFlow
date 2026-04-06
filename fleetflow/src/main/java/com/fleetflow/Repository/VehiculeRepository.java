a4aa0d7796dc7028f15a4fe5c98611a02227620f
HibaHAJAJI <wiamhiba08@gmail.com>
2026-04-06 15:42:05 +0100

    Clean up VehiculeRepository by removing queries
    Removed unused query annotations from VehiculeRepository.

--- fleetflow/src/main/java/com/fleetflow/Repository/VehiculeRepository.java
+++ fleetflow/src/main/java/com/fleetflow/Repository/VehiculeRepository.java
@@ -10,11 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface VehiculeRepository extends JpaRepository<Vehicule, Long> {
-
-    @Query("select  v from Vehicule v where v.statut =:statut ")
     List<Vehicule>findByStatut(@Param("statut") StatutVehicule statut);
-
-    @Query("select v from Vehicule  v where v.capacite> :seuil")
     List<Vehicule>findByCapaciteGreaterThan(@Param("seuil")double seuil);
 
 }
